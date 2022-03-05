aef2df70b27628a6275c94afaec80c3ca4c3a5f1
Seth <jonRiemer.88@outlook.com>
2022-03-04 23:07:40 -0500

    completed basis features

--- app/src/main/java/com/example/flashcard/MainActivity.java
+++ app/src/main/java/com/example/flashcard/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.flashcard;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView flashcardQuestion = findViewById(R.id.flashcard_question_textview);
+        TextView flashcardAnswer = findViewById(R.id.flashcard_answer_textview);
+
+        flashcardQuestion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                flashcardAnswer.setVisibility(View.VISIBLE);
+                flashcardQuestion.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        flashcardAnswer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                flashcardQuestion.setVisibility(View.VISIBLE);
+                flashcardAnswer.setVisibility(View.INVISIBLE);
+            }
+        });
+
     }
 }
