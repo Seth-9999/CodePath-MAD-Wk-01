f80e14b4de6d091f69fc67f0c92251b916bbd115
Seth <jonRiemer.88@outlook.com>
2022-03-18 23:41:37 -0400

    have error

--- app/src/main/java/com/example/flashcard/MainActivity.java
+++ app/src/main/java/com/example/flashcard/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.flashcard;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -32,8 +35,29 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        ImageView addFlashcardButton = findViewById(R.id.add_flashcard);
+
+        addFlashcardButton.setOnClickListener(view -> {
+            //Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+            Intent intentAddFlashcard = new Intent(this, MainActivity2.class);
+            startActivity(intentAddFlashcard);
+            //startActivityForResult(intent, 100);
+        });
 
 
 
     }
+
+    /*
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == 100) {
+            String cardQuestion = data.getExtras().getString("cardQuestion");
+            String cardAnswer = data.getExtras().getString("cardAnswer");
+        }
+    }
+    */
+
+
 }
---
+++ app/src/main/java/com/example/flashcard/MainActivity2.java
@@ -0,0 +1,51 @@
+package com.example.flashcard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+
+        EditText new_flashcard_text = findViewById(R.id.new_flashcard_question);
+        EditText new_flashcard_answer = findViewById(R.id.new_flashcard_answer);
+
+        ImageView save_flashcard_icon = findViewById(R.id.add_flashcard);
+        ImageView cancel_flashcard_icon = findViewById(R.id.cancel_flashcard);
+
+        cancel_flashcard_icon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("Cancel button clicked");
+            }
+        });
+
+
+        save_flashcard_icon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("Save button clicked");
+
+
+               // String question = new_flashcard_text.getText().toString();
+                //String answer = new_flashcard_answer.getText().toString();
+
+            }
+        });
+
+
+
+    }
+
+
+}
